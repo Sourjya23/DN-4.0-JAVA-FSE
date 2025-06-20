88abf1269686131eced8116eae9d15c956cdc539
Sourjya Mitra <162135446+Sourjya23@users.noreply.github.com>
2025-06-20 19:57:50 +0530

    Added Week-1 Exercises

--- Exercises/Week-1/Data-structure-and-algorithms/Exercise-7/forecasting/src/forecasting/Test.java
+++ Exercises/Week-1/Data-structure-and-algorithms/Exercise-7/forecasting/src/forecasting/Test.java
@@ -2,12 +2,12 @@ package forecasting;
 
 public class Test {
 	 public static void main(String[] args) {
-	        double currentValue = 10000; // e.g., ₹10,000
-	        double growthRate = 0.1;     // 10% growth
-	        int years = 5;
+	        double currentValue = 15000; // e.g., ₹15,000
+	        double growthRate = 0.2;     // 20% growth
+	        int years = 3;
 
 	        double futureValue = Forecast.predictFutureValue(currentValue, growthRate, years);
-	        System.out.printf("Predicted value after %d years: ₹%.2f%n", years, futureValue);
+	        System.out.printf("Predicted future value after %d years: ₹%.2f%n", years, futureValue);
 	    }
 
 }
